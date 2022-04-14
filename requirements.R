1b7f05ea274d141421af9ee68d2b261daccffa25
YegorGalkin <radiation7@protonmail.com>
2022-04-14 14:52:13 +0200

    Added PCA and PRCALC results + package requirement file

---
+++ requirements.R
@@ -0,0 +1,9 @@
+packages <- c('tidyverse','ggpubr','Gifi','knitr')
+
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
